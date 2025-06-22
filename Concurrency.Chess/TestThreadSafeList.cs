eab3823316b02fbfca2d0c0093d738e0c948f4c1
unknown <joseph@odd-e.com>
2025-06-22 16:28:39 +0800

    implement code and chess tests for ThreadSafeList but only IList interface methods

---
+++ Concurrency.Chess/TestThreadSafeList.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Concurrency.TestTools.UnitTesting;
+using Microsoft.Concurrency.TestTools.UnitTesting.Chess;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Concurrency.Chess
+{
+    [TestFixture]
+    [ChessInstrumentAssembly("mscorlib")]
+    [ChessInstrumentAssembly("nunit.framework", Exclude = true)]
+    public class TestThreadSafeList
+    {
+        [Test]
+        [DataRaceTestMethod]
+        [RegressionTestExpectedResult(TestResultType.Passed)]
+        public void TestPassedConcurrentEnumerateReadAndUpdateWithThreadSafeList()
+        {
+            var cList = new ThreadSafeList<int>(new List<int> { 1, 2, 3, 4 });
+            var listNode = cList[2];
+
+            Thread t = new Thread(
+                () =>
+                {
+                    var enumerater = cList.GetEnumerator();
+                    while (enumerater.MoveNext())
+                    {
+                        int current = enumerater.Current;
+                    }
+                });
+            t.Start();
+
+            cList.Remove(listNode);
+            t.Join();
+        }
+
+        [Test]
+        [DataRaceTestMethod]
+        [RegressionTestExpectedResult(TestResultType.Passed)]
+        public void TestPassedConcurrentLoopReadAndUpdateWithThreadSafeList()
+        {
+            var cList = new ThreadSafeList<int>(new List<int> { 1, 2, 3, 4 });
+            var listNode = cList[2];
+
+            Thread t = new Thread(
+                () =>
+                {
+                    foreach (var v in cList)
+                    {
+                        object v1 = v;
+                    }
+                });
+            t.Start();
+
+            cList.Remove(listNode);
+            t.Join();
+        }
+
+    }
+}
---
+++ Concurrency/ThreadSafeList.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concurrency
+{
+    public class ThreadSafeList<T> : IList<T>
+    {
+        private List<T> _list;
+        private object _root;
+
+        public ThreadSafeList(List<T> list)
+        {
+            _list = list;
+            _root = ((ICollection)list).SyncRoot;
+        }
+
+        public int Count
+        {
+            get
+            {
+                lock (_root)
+                {
+                    return _list.Count;
+                }
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return ((ICollection<T>)_list).IsReadOnly;
+            }
+        }
+
+        public void Add(T item)
+        {
+            lock (_root)
+            {
+                _list.Add(item);
+            }
+        }
+
+        public void Clear()
+        {
+            lock (_root)
+            {
+                _list.Clear();
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            lock (_root)
+            {
+                return _list.Contains(item);
+            }
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            lock (_root)
+            {
+                _list.CopyTo(array, arrayIndex);
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            lock (_root)
+            {
+                return _list.Remove(item);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            lock (_root)
+            {
+                var copy = new List<T>(_list);
+                return copy.GetEnumerator();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            lock (_root)
+            {
+                var copy = new List<T>(_list);
+                return ((IEnumerable<T>)copy).GetEnumerator();
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                lock (_root)
+                {
+                    return _list[index];
+                }
+            }
+            set
+            {
+                lock (_root)
+                {
+                    _list[index] = value;
+                }
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            lock (_root)
+            {
+                return _list.IndexOf(item);
+            }
+        }
+
+        public void Insert(int index, T item)
+        {
+            lock (_root)
+            {
+                _list.Insert(index, item);
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            lock (_root)
+            {
+                _list.RemoveAt(index);
+            }
+        }
+    }
+}
